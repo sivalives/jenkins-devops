56ea971f48796f7440be11781f71e7a82569c8a8
Siva Anand <Siva@Sivas-iMac.local>
2023-10-21 14:10:30 +0530

    Moving creds to env for testing

--- basic-security.groovy
+++ basic-security.groovy
@@ -3,12 +3,15 @@
 import jenkins.model.*
 import hudson.security.*
 
+def username = System.getenv("JENKINS_USERNAME")
+def password = System.getenv("JENKINS_PASSWORD")
+
 def instance = Jenkins.getInstance()
 
 println "--> creating local user 'admin'"
 
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
-hudsonRealm.createAccount('admin','admin')
+hudsonRealm.createAccount(username,password)
 instance.setSecurityRealm(hudsonRealm)
 
 def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
